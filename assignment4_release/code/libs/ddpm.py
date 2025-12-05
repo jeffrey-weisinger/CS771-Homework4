2b78ca93e799a06c3953972af5b6123f371cac20
fpeterson20 <fpeterson2001@gmail.com>
2025-12-05 12:29:09 -0600

    Updated integer bug in image generation

--- assignment4_release/code/libs/ddpm.py
+++ assignment4_release/code/libs/ddpm.py
@@ -253,8 +253,9 @@ class DDPM(nn.Module):
         # essentially, we are converting the entire img tensor, one timestep at a time.
 
         labels = torch.tensor(labels, device=device)
+
         for t_index in range(self.timesteps-1, -1, -1):
-            t = torch.ones(shape[0], device=device) * t_index
+            t = torch.ones(shape[0], dtype=int, device=device) * t_index
             imgs = self.p_sample(imgs, labels, t, t_index)
 
         # postprocessing the images
